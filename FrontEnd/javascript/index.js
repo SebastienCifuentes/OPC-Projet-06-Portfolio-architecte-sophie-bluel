c31c32d2dd6f85215723638cf1c723c40a499645
SebastienC <sebastien.cifuentes@gmail.com>
2023-09-08 10:43:56 +0200

    Récupération des travaux depuis l'API, ajout en JS  et suppression du html correspondant

---
+++ FrontEnd/javascript/index.js
@@ -0,0 +1,29 @@
+//Récupération des travaux depuis l'API : fetch GET
+fetch("http://localhost:5678/api/works")
+    .then((response) => response.json())
+    .then((works) => displayWorks(works))
+    .catch((error) => {
+        alert(`Erreur: ` + error);
+});
+
+function displayWorks(works) {
+    for (let i = 0; i < works.length; i++) {
+        const worksIndex = works[i];
+        // Récupération de la section du Dom pour afficher la galerie
+        const sectionGallery = document.querySelector(".gallery");
+        // Création de la balise figure qui affichera les travaux
+        const worksElement = document.createElement("figure");
+        // Création des balises interne qui affichera images et titres
+        const imageElement = document.createElement("img");
+        imageElement.src = worksIndex.imageUrl;
+        imageElement.alt = worksIndex.title;
+        const titleElement = document.createElement("h3");
+        titleElement.innerHTML = worksIndex.title;
+
+        // Rattachement de la balise figure à la section gallery
+        sectionGallery.appendChild(worksElement);
+        // Rattachement des balises img et h3 à la balise figure
+        worksElement.appendChild(imageElement);
+        worksElement.appendChild(titleElement);
+    }
+}
