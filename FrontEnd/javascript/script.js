5ca4dd15b0734078b5521109194cb274fe63dc31
SebastienC <sebastien.cifuentes@gmail.com>
2023-09-28 21:03:32 +0200

    maj script js

--- FrontEnd/javascript/script.js
+++ FrontEnd/javascript/script.js
@@ -1,14 +1,23 @@
+// On récupère le token
 const token = localStorage.getItem('token');
 
+// Si token stocké, donc login ok
 if (token) {
+// On joute la barre admin et on enlève les filtres
   document.querySelector('.blackBarAdmin').classList.remove('hidden');
   document.querySelector('.filters').classList.add('hidden');
+
+  
+
+  // On modifie la valeur du bouton login en logout
   const loginBtn = document.querySelector('.loginLogout');
   loginBtn.innerHTML = 'logout';
+
+  // Au clic sur logout on se délog (enlève le token) et on revient sur la page index
   loginBtn.href = '#';
   loginBtn.addEventListener('click', (event) => {
     event.preventDefault();
     window.localStorage.removeItem('token');
-    window.location = "index.html";
-  })
+    window.location = 'index.html';
+  });
 }
