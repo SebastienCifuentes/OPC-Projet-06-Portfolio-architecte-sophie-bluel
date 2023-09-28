743c051446bd931ac4d49c2466c6fed44eecfc42
SebastienC <sebastien.cifuentes@gmail.com>
2023-09-28 18:05:45 +0200

    Mise en place du login et log out

--- FrontEnd/javascript/script.js
+++ FrontEnd/javascript/script.js
@@ -1,9 +1,14 @@
 const token = localStorage.getItem('token');
 
-if (token){
-    document.querySelector('.blackBarAdmin').classList.remove('hidden');
-    document.querySelector('.filters').classList.add('hidden');
-}
-else{
-    
+if (token) {
+  document.querySelector('.blackBarAdmin').classList.remove('hidden');
+  document.querySelector('.filters').classList.add('hidden');
+  const loginBtn = document.querySelector('.loginLogout');
+  loginBtn.innerHTML = 'logout';
+  loginBtn.href = '#';
+  loginBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.localStorage.removeItem('token');
+    window.location = "index.html";
+  })
 }
