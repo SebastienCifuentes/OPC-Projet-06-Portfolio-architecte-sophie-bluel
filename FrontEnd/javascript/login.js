da7db225c8246c96c5fa08c6d84edea12a43ac1b
SebastienC <sebastien.cifuentes@gmail.com>
2023-10-23 00:12:28 +0200

    MAJ ES6 et commentaires page JS Login

--- FrontEnd/javascript/login.js
+++ FrontEnd/javascript/login.js
@@ -1,31 +1,22 @@
+// Sélectionne le formulaire dans le DOM.
 const form = document.querySelector('form');
 
-let formData;
-
-form.addEventListener('submit', (e) => {
+// Ajoute un événement 'submit' au formulaire.
+form.addEventListener('submit', async (e) => {
+  // Reset du formulaire.
   e.preventDefault();
-  formData = new FormData(form);
-  // on ajoute l'email et le mdp dans le formData
-  formData.append('email', document.querySelector('input[type=email]').value);
-  formData.append(
-    'password',
-    document.querySelector('input[type=password]').value
-  );
-
-  // on créé un objet login avec l'email et le mdp
-  const login = {
-    email: formData.getAll('email')[0],
-    password: formData.getAll('password')[0],
-  };
-
-  postLogin(login); // Appel de la fonction qui gère le POST
-
   
-});
+  // Récupère les valeurs des email et mot de passe du formulaire.
+  const email = document.querySelector('input[type=email]').value;
+  const password = document.querySelector('input[type=password]').value;
 
-async function postLogin(login) {
-  try { // on essaye le post
-    let reponse = await fetch('http://localhost:5678/api/users/login', {
+  // Crée un objet login avec les valeurs récupérées.
+  const login = { email, password };
+
+  try {
+
+    // Envoie une requête POST au serveur avec les données de login.
+    const response = await fetch('http://localhost:5678/api/users/login', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
@@ -34,17 +25,21 @@ async function postLogin(login) {
       body: JSON.stringify(login),
     });
 
-    let resultat = await reponse.json();
-    let statusRequete = reponse.status;
+    // Attend que la réponse du serveur soit convertie en JSON.
+    const result = await response.json();
 
-    if (statusRequete === 200) { // Si status reponse 200 (ok), alors on stock le token et redirection homepage
-      localStorage.setItem('token', resultat.token);
+    // Vérifie si la réponse du serveur est OK (code statut HTTP entre 200 et 299).
+    if (response.ok) {
+      // Si c'est OK, stocke le token reçu dans le localStorage.
+      localStorage.setItem('token', result.token);
+      // Redirige l'utilisateur vers la page d'accueil.
       window.location = './index.html';
-    } else { // Sinon message d'erreur
+    } else {
+      // Si la réponse n'est pas OK, affiche un message d'erreur.
       document.querySelector('.errorMessage').classList.remove('hidden');
     }
   } catch (error) {
+    // Si erreur lors de l'exécution du code dans le try, affiche l'erreur dans la console.
     console.log(error);
   }
-}
-
+});
